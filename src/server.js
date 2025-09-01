f0a067ab8407d20a0408030dfbf8bee315c5a45f
KissAkos <kopyherceg@gmail.com>
2025-09-01 20:07:20 +0300

    app

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 
-const express = require('express');
+const app = require('./app')
 
-const app = express();
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
